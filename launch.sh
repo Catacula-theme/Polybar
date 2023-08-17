eb4395f232866d0c73b75467af34b4b15548e214
qapfuc <qapfuc@gmail.com>
2023-08-17 15:58:38 +0300

    polybar

---
+++ launch.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+polybar mono -c $HOME/.config/polybar/config.ini &
---
+++ scripts/currencyrates.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json,sys,urllib.request
+
+file = open("ex.txt")
+
+if len(sys.argv) != 3:
+    print("Usage: ./currencyrates.py comma_separated_currency_list base_currency. Example: ./currencyrates.py usd,eur,btc nok")
+    sys.exit()
+
+currencies = sys.argv[1]
+basecurrency = sys.argv[2]
+
+currencyurl = "http://freecurrencyrates.com/api/action.php?do=cvals&iso=" + currencies.replace(',','') + "&f=" + basecurrency + "&v=1&s=cbr"
+f = urllib.request.urlopen(currencyurl)
+obj = json.loads(f.read())
+res="";
+for c in currencies.split(','):
+    res += c.upper() + ":{:,.2f}".format(1/obj[c.upper()]).replace(',',' ')
+
+# some unicode currency code replacement (optional)
+res = res.replace("USD", "$")
+res = res.replace("EUR", "€")
+res = res.replace("BTC", "")
+
+file.write(res)
+ex = file.read()
+sys.stdout.write(ex);
